c48e0b9d4016dbd69bee7ba88fe67bf4c87dbe76
FatemehGheysari <fateme.gheysari8686@gmail.com>
2025-07-31 13:46:26 +0200

    chore: migrate vitest config to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-// vitest.config.ts
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/resume-template/',
+    plugins: [react()],
     test: {
         globals: true,
         environment: 'jsdom',
         setupFiles: './tests/setup.ts',
     },
 });
-
